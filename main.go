ef03245f50fef3a99da2837e9c1a46b58d2a9d49
alorents <andreasphoenix@gmail.com>
2023-05-03 20:03:31 -0700

    add page not found default path

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		// TODO: handle the page not found error
+		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
