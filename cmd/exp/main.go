1c202dc2eef388f938b7975571b78c53bf2ddb4b
alorents <andreasphoenix@gmail.com>
2023-05-09 12:01:38 -0700

    use pgx to configure a postgres db driver

---
+++ cmd/exp/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
+}
+
+func main() {
+	postgresConfig := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		DBName:   "lenslocked",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", postgresConfig.String())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Connected")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/alorents/lenslocked
 
 go 1.20
 
-require github.com/go-chi/chi/v5 v5.0.8
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
+)
