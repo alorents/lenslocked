113d150bc0358aee449090fdfa19749b5a402448
alorents <andreasphoenix@gmail.com>
2023-05-10 15:33:59 -0700

    add UserService to users controller

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/alorents/lenslocked/controllers"
+	"github.com/alorents/lenslocked/models"
 	"github.com/alorents/lenslocked/templates"
 	"github.com/alorents/lenslocked/views"
 )
@@ -21,7 +22,19 @@ func main() {
 	router.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "layout.gohtml", "signup.gohtml"))))
 	router.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "layout.gohtml", "faq.gohtml"))))
 
-	usersC := controllers.Users{}
+	postgresConfig := models.DefaultPostgresConfig()
+	db, err := models.Open(postgresConfig)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	us := models.UserService{
+		DB: db,
+	}
+	usersC := controllers.Users{
+		UserService: &us,
+	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "layout.gohtml", "signup.gohtml"))
 	router.Get("/signup", usersC.New)
 	router.Post("/users", usersC.Create)
