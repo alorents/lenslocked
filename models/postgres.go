6ff0e32e876942b144bcbc6ee677a68d991d3114
alorents <andreasphoenix@gmail.com>
2023-05-11 16:40:31 -0700

    run migrations with goose at server start

--- models/postgres.go
+++ models/postgres.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 func DefaultPostgresConfig() PostgresConfig {
@@ -25,6 +26,14 @@ func Open(config PostgresConfig) (*sql.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error opening database: %w", err)
 	}
+	defer db.Close()
+
+	// run migrations
+	err = Migrate(db, "./migrations")
+	if err != nil {
+		panic(err)
+	}
+
 	return db, db.Ping()
 }
 
@@ -39,4 +48,17 @@ type PostgresConfig struct {
 
 func (cfg PostgresConfig) String() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
+}
+
+// Migrate will attempt to run all of the migrations using goose
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("error setting dialect: %w", err)
+	}
+	err = goose.Up(db, dir)
+	if err != nil {
+		return fmt.Errorf("error running migrations: %w", err)
+	}
+	return nil
 }
