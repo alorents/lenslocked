f406f982379a5ef5c6f649fde89ca33ca6f56dc2
alorents <andreasphoenix@gmail.com>
2023-05-10 20:51:01 -0700

    stub out the session service

--- models/session.go
+++ models/session.go
@@ -1,7 +1,31 @@
 package models
 
+import (
+	"database/sql"
+)
+
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is only set when creating a new session
+	// Only the TokenHash will be saved in the DB
+	// So at all other times Token will be empty
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (s *SessionService) Create(userID int) (*Session, error) {
+	// TODO create the sesssion token
+
+	// TODO Implement SessionService.Create
+	return nil, nil
+}
+
+func (s *SessionService) User(token string) (*User, error) {
+	// TODO Implement SessionService.User
+	return nil, nil
 }
