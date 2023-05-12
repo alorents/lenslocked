c8e47eb7c8ddc0aecf2bd5344c5faaf751fb1a82
alorents <andreasphoenix@gmail.com>
2023-05-12 12:29:59 -0700

    create env vars file and load them using github.com/joho/godotenv

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 
 	"github.com/alorents/lenslocked/models"
 )
 
-// Mailtrap credentials
-const (
-	host     = "sandbox.smtp.mailtrap.io"
-	port     = 587
-	username = "e3d8389ef2588d"
-	password = "ccc570e0112cd6"
-)
-
 func main() {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the SMTP config from the environment
+	host := os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+
 	es := models.NewEmailService(models.SMTPConfig{
 		Host:     host,
 		Port:     port,
 		Username: username,
 		Password: password,
 	})
-	err := es.ForgotPassword("andreasphoenix@gmail.com", "fakeresettoken")
+	err = es.ForgotPassword("andreasphoenix@gmail.com", "fakeresettoken")
 	if err != nil {
 		panic(err)
 	}
