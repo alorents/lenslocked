9415b6969ecae9b792eb8d52d7fd406bae3d2c06
Andreas Lorentson <andreasphoenix@gmail.com>
2024-09-25 22:57:57 -0700

    adds ImagesDir to GalleryService

--- models/gallery.go
+++ models/gallery.go
@@ -3,6 +3,7 @@ package models
 import (
 	"database/sql"
 	"fmt"
+	"path/filepath"
 
 	"github.com/alorents/lenslocked/errors"
 )
@@ -15,6 +16,11 @@ type Gallery struct {
 
 type GalleryService struct {
 	DB *sql.DB
+
+	// ImagesDir is used to tell the GalleryService where to store and locate
+	// images. If not set, the GalleryService will default to using the "images"
+	// directory.
+	ImagesDir string
 }
 
 func (service *GalleryService) Create(title string, userID int) (*Gallery, error) {
@@ -96,4 +102,12 @@ func (service *GalleryService) Delete(id int) error {
 		return fmt.Errorf("delete gallery by id: %w", err)
 	}
 	return nil
+}
+
+func (service GalleryService) galleryDir(id int) string {
+	imagesDir := service.ImagesDir
+	if imagesDir == "" {
+		imagesDir = "images"
+	}
+	return filepath.Join(imagesDir, fmt.Sprintf("gallery-%d", id))
 }
