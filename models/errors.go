7740ed92fb1b967ea25b9881d638c2d94874ec07
Andreas Lorentson <andreasphoenix@gmail.com>
2024-11-14 21:58:39 -0800

    deetect content type

--- models/errors.go
+++ models/errors.go
@@ -2,9 +2,51 @@ package models
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"path/filepath"
 )
 
 var (
 	ErrEmailTaken = errors.New("models: email address is already in use")
 	ErrNotFound   = errors.New("models: resoures could not be found")
 )
+
+type FileError struct {
+	Issue string
+}
+
+func (fe FileError) Error() string {
+	return fmt.Sprintf("invalid file: %v", fe.Issue)
+}
+
+func checkContentType(r io.ReadSeeker, allowedTypes []string) error {
+	testBytes := make([]byte, 512)
+	_, err := r.Read(testBytes)
+	if err != nil {
+		return fmt.Errorf("checking content type: %w", err)
+	}
+
+	_, err = r.Seek(0, 0)
+	if err != nil {
+		return fmt.Errorf("checking content type: %w", err)
+	}
+
+	contentType := http.DetectContentType(testBytes)
+	for _, allowedType := range allowedTypes {
+		if contentType == allowedType {
+			return nil
+		}
+	}
+
+	return FileError{Issue: fmt.Sprintf("content type: %s is not allowed", contentType)}
+}
+
+func checkExtenstion(filename string, allowedExtensions []string) error {
+	if hasExtension(filename, allowedExtensions) {
+		return nil
+	}
+
+	return FileError{Issue: fmt.Sprintf("file extension: %s is not allowed", filepath.Ext(filename))}
+}
